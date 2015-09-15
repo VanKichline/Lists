4d5c2f57fd077c5f56c27f2f18b5274343b5ad29
Van Kichline <vkichline@hotmail.com>
2015-09-15 16:43:42 -0700

    Some data access working

--- src/Lists/React/listsApp.jsx
+++ src/Lists/React/listsApp.jsx
@@ -1,11 +1,54 @@
 ﻿var MainPane = React.createClass({
     render: function () {
+        var todoList = this.props.data.map(function (item) {
+            return <Item ItemText={item.ItemText} Done={item.Done} />
+        });
         return (
             <div className="container">
-                <div>This is the main container.</div>
+                <div className="row">
+                    <h1>Todo List</h1>
+                </div>
+                <div className="row">
+                    <table className="todoTable">
+                        {todoList}
+                    </table>
+                </div>
             </div>
         );
     }
 });
 
-React.render(<MainPane />, document.getElementById('content'));
+// Props: ItemText, Done
+var Item = React.createClass({
+    render: function () {
+        return (
+            <tr>
+                <td className="cbTD"><input type="checkbox" checked={this.props.Done} /></td>
+                <td>{this.props.ItemText}</td>
+            </tr>
+        );
+    }
+});
+
+var App = React.createClass({
+    getInitialState: function () {
+        return { data: [] };
+    },
+    componentWillMount: function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', this.props.url, true);
+        xhr.onload = function () {
+            var webAPIData = JSON.parse(xhr.responseText);
+            this.setState({ data: webAPIData });
+        }.bind(this);
+        xhr.send();
+    },
+    render: function () {
+        return (
+           <MainPane data={this.state.data} />
+        );
+    }
+});
+
+
+React.render(<App url="/api/items/" />, document.getElementById('content'));
