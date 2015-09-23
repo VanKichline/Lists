c9bf2edfc53e6eded47967fe4a558ad54695b7eb
Van Kichline <vkichline@hotmail.com>
2015-09-23 11:14:43 -0700

    Forced migrations to compile: do not merge.  Added site.css.

--- src/Lists/Migrations/20150914172035_Initial.Designer.cs
+++ src/Lists/Migrations/20150914172035_Initial.Designer.cs
@@ -1,13 +1,10 @@
-using System;
+using Lists.Models;
 using Microsoft.Data.Entity;
 using Microsoft.Data.Entity.Infrastructure;
 using Microsoft.Data.Entity.Metadata;
 using Microsoft.Data.Entity.Migrations;
-using Lists.Models;
-using Microsoft.Data.Entity.SqlServer.Metadata;
 
-namespace Lists.Migrations
-{
+namespace Lists.Migrations {
     [DbContext(typeof(ItemContext))]
     [Migration("20150914172035_Initial")]
     partial class Initial
